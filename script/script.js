7f8a721d51cbd096e213368b38f87cb5ccf3c28c
ianfelps <iannfelps@gmail.com>
2025-01-08 15:16:38 -0300

    update 2025

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+// efeito de fade-in
 document.addEventListener("DOMContentLoaded", () => {
     const elements = document.querySelectorAll(".fade-in");
 
@@ -14,3 +15,23 @@ document.addEventListener("DOMContentLoaded", () => {
         fadeInObserver.observe(element);
     });
 });
+
+// botão de rolar para o topo
+document.addEventListener('DOMContentLoaded', function () {
+    var scrollToTopBtn = document.getElementById('scrollToTopBtn');
+  
+    window.addEventListener('scroll', function () {
+      // mostra ou oculta o botão dependendo da posição do scroll
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopBtn.classList.add('visible');
+      } else {
+        scrollToTopBtn.classList.remove('visible');
+      }
+    });
+  
+    // adiciona um listener para rolar suavemente ao topo
+    scrollToTopBtn.addEventListener('click', function () {
+      document.body.scrollTop = 0; // para browsers da web
+      document.documentElement.scrollTop = 0; // para browsers modernos
+    });
+  });
